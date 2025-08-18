e96a46c6360d7f64035d8f46e116d70bf085630d
Abdul Waleed <abdulwaleedfsd@gmail.com>
2025-08-18 01:13:00 -0700

    created and tested test api

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,9 @@
+// import bcryptjs from "bcryptjs";
+// import jwt from "jsonwebtoken";
+// import User from "../models/user.model.js";
+// import { errorHandler } from "../utils/error.js";
+
+// test api
+export const test = (req, res) => {
+  res.json({ message: "API is working properly." });
+};
--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 // import path from "path";
 // import cookieParser from "cookie-parser";
 // import { fileURLToPath } from "url";
-// import userRoutes from "./routes/user.route.js";
+import userRoutes from "./routes/user.route.js";
 // import authRoutes from "./routes/auth.route.js";
 // import productRoutes from "./routes/product.route.js";
 
@@ -41,7 +41,7 @@ const app = express();
 // Now URLs like http://localhost:5173/uploads/filename.jpg will work
 
 // routes
-// app.use("/backend/user", userRoutes);
+app.use("/backend/user", userRoutes);
 // app.use("/backend/auth", authRoutes);
 // app.use("/backend/product", productRoutes);
 
