ead19bebc900b596cf75a32cc6970cd604e036d1
Abdul Waleed <abdulwaleedfsd@gmail.com>
2025-08-18 01:00:04 -0700

    set the folder structure for frontend and backend folders and enabled react-router-dom in root file of frontend folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import SignUp from "./pages/SignUp.jsx";
+import SignIn from "./pages/SignIn.jsx";
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
 
 const App = () => {
-  return <div className="text-red-500">app</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+      </Routes>
+
+      <Footer />
+
+      {/* Toast container to show notifications */}
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        pauseOnHover
+        draggable
+        theme="colored"
+        style={{ marginTop: "55px" }}
+      />
+    </BrowserRouter>
+  );
 };
 
 export default App;
